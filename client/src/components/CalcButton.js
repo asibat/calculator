84d520c1a760bbc842fe2c6261257131048d6600
asibat <asibat@wiser.com>
2019-06-08 20:58:52 +0300

    Ok, im done playing with heroku and touch events :)

--- client/src/components/CalcButton.js
+++ client/src/components/CalcButton.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 
+import PointTarget from 'react-point'
+
 export default class CalcButton extends Component {
   render() {
     const { onPress, className, ...props } = this.props
 
     return (
-      <button className={`calculator-key ${className}`} {...props} onTouchStart={onPress}>
-        {props.screenvalue}
-      </button>
+      <PointTarget onPoint={onPress}>
+        <button className={`calculator-key ${className}`} {...props}>
+          {props.screenvalue}
+        </button>
+      </PointTarget>
     )
   }
 }
